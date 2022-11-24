db6e99a73e3492f22ca719f01e70a0bee45db946
homerze <homerze@mfcgd.com>
2022-11-24 10:08:41 +0800

    typeDefs

---
+++ typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    users(id: ID!): Users
+  }
+  type Users {
+    id: ID
+    firstname: String
+    lastname: String
+    password: String
+    username: String
+    age: Int
+    walkingAverage: String
+    # not sure with this
+    activity: [Activity]
+    sleep: [Sleep]
+  }
+  type Sleep {
+    id: ID
+    userId: ID
+    sleepStart: String
+    sleepEnded: String
+    oxygenSaturation: String
+    avgHeartRate: String
+    maxHeartRate: String
+    sleepHours: String
+    remSleep: String
+    sleepLevel: Int
+    datetime: String
+  }
+  type Activity {
+    id: ID
+    userId: ID
+    steps: String
+    intensity: String
+    oxygenSaturation: String
+    datetime: String
+  }
+`;
+
+module.exports = typeDefs;
